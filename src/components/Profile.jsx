b4f8687a697630289d997af00f30355626ac048e
Moriyel <moriyel@rambler.ru>
2020-11-07 21:53:53 +0300

    start social

---
+++ src/components/Profile.jsx
@@ -0,0 +1,31 @@
+
+
+const Profile = () => {
+  return   (  
+  <div className= 'content'>
+  <div>
+      <img src = 'https://nashaplaneta.net/articles/images/xindonesia-alishha-2.jpg.pagespeed.ic.SvTFiGyI68.jpg' />
+  </div>
+  <div>
+      ava +
+  </div>
+  <div>
+    my posts
+      <div>
+        New posts
+      </div>
+      <div>
+        <div>
+          post1
+        </div>
+        <div>
+          post2
+        </div>
+      </div>
+  </div>
+</div> 
+  );
+}
+
+
+export default Profile;
