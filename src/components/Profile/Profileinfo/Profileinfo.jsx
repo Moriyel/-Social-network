93fd4b11262b0172410dd3abfb3d5dc6380275d8
Moriyel <moriyel@rambler.ru>
2020-12-25 23:15:27 +0300

    'status'

--- src/components/Profile/Profileinfo/Profileinfo.jsx
+++ src/components/Profile/Profileinfo/Profileinfo.jsx
@@ -1,5 +1,6 @@
 import Preloader from '../../common/Preloader/Preloader';
 import a from './Profileinfo.module.css';
+import ProfileStatus from './Profilestatus'; 
 
 const ProfileInfo = (props) => {
   if (!props.profile) {//если нет профайла то отрисуй без него перезагрузку
@@ -7,12 +8,12 @@ const ProfileInfo = (props) => {
   }
   return   (  
   <div>
-      <div>
+      {/*<div>
           <img src = 'https://nashaplaneta.net/articles/images/xindonesia-alishha-2.jpg.pagespeed.ic.SvTFiGyI68.jpg' />
-      </div>
+      </div>*/}
       <div className = {a.descriptionBlock}>
           <img src={props.profile.photos.large} />
-          ava +
+          <ProfileStatus status = {"Hello"} />
       </div>
 </div> 
   );
