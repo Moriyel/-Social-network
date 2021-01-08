07d096d82d111f50e3739db66aabeda786d6cbb5
Moriyel <moriyel@rambler.ru>
2021-01-08 18:14:59 +0300

    'form_login'

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -4,13 +4,15 @@ import messageReducer from "./message-reducer";
 import postReducer from "./profile-reducer";
 import usersReducer from "./users-reducer";
 import thunkMiddleware from 'redux-thunk';
+import {reducer as formReducer} from 'redux-form';
 
 
 let reducers = combineReducers({
   allPosts: postReducer,
   allMessages: messageReducer,
   allUsers: usersReducer,
-  auth: authReducer
+  auth: authReducer,
+  form: formReducer
   
 });
 
